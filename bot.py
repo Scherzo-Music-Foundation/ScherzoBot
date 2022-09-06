4d3427fe19ac81da646a4eb267fa88eca549f7b3
Scherzo-Music-F <112141568+Scherzo-Music-Foundation@users.noreply.github.com>
2022-09-06 02:07:49 +0000

    initial bot

--- bot.py
+++ bot.py
@@ -1,10 +1,18 @@
-import discord
+from discord import Client, Intents, Status, Activity, ActivityType
 import os
 
-client = discord.Client(intents=discord.Intents.default())
+client = Client(intents=Intents.default())
 
 @client.event
 async def on_ready():
-	print("Ready")
+  print("Ready")
+  await client.change_presence(status=Status.online,activity=Activity(type=ActivityType.watching, name="over Scherzo!"))
+
+@client.event
+async def on_message(ctx):
+  if ctx.guild.id == 1016518413209837670 and not ctx.author.bot:
+    await ctx.channel.send("hi")
+  
+
 
 TOKEN = os.environ["TOKEN"]
--- sustain.py
+++ sustain.py
@@ -10,7 +10,8 @@ def home():
 def run():
   app.run(
 		host='0.0.0.0',
-		port=8080
+		port=8080,
+    debug=True
 	)
 
 def sustain():
